3e9e90ae133e33887a5f5607d45410852dca5b9c
taibaiyinxing <yuguotao1989@126.com>
2016-06-07 10:24:11 +0800

    first commit

---
+++ app/src/main/java/com/taibai/yugt/androidbacklist/MainActivity.java
@@ -0,0 +1,33 @@
+package com.taibai.yugt.androidbacklist;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    int REQUEST_PHONE_PERMISSION = 0;
+    int REQUEST_BOOT_COMPLETED_PERMISSION = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_PHONE_STATE)) {
+                Toast.makeText(this, "申请个权限", Toast.LENGTH_SHORT).show();
+            }
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, REQUEST_PHONE_PERMISSION);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/taibai/yugt/androidbacklist/reciever/PhoneReciever.java
@@ -0,0 +1,7 @@
+package com.taibai.yugt.androidbacklist.reciever;
+
+/**
+ * Created by yugt on 16/6/6.
+ */
+public class PhoneReciever {
+}
